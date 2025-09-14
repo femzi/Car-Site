2459d3cb9489024a51f8ceb60909e1096f026e85
femzi <108510189+femzi@users.noreply.github.com>
2025-09-14 23:22:17 +0100

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
-
-
+import './index.css'
+import TargetCursor from './components/TargetCursor';
 function App() {
 
 
   return (
     <>
-<h1 className="text-3xl font-bold underline">
-  Lets Go!
+<div className="row pt-6 pl-10 pb-3 bg-[#0F0F0F] text-white font-mono"> 
+<div>
+    <h1 className="text-2xl  font-bold mr-45 ">
+ LUXE MOTION
   </h1>
+</div>
+<div>
+
+      <TargetCursor 
+        spinDuration={2}
+        hideDefaultCursor={true}
+      />
+      
+<ul className="row">
+  <li className=" cursor-target hovering" ><a href="">Home</a></li>
+    <li className="ml-15 cursor-target hovering" ><a href="">Our Services</a></li>
+      <li className="ml-15 cursor-target hovering" ><a href="">About Us</a></li>
+        <li className="ml-15 cursor-target hovering" ><a href="">Why Us?</a></li>
+  <li className="ml-15 cursor-target hovering" ><a href="">Contact Us</a></li>
+</ul>
+</div>
+  
+</div>
     </>
   )
 }
