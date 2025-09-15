9faab8af691bb8921a797515ddaa47659855a3d1
femzi <108510189+femzi@users.noreply.github.com>
2025-09-15 17:41:13 +0100

    about

---
+++ src/About.tsx
@@ -0,0 +1,38 @@
+
+ 
+
+function About (){
+    return (
+        <>
+       
+       
+        <div className=" pl-14 bg-[#0F0F0F] pb-20 text-[#FFFFFF] font-mono">
+        <p className="text-4xl font-bold">
+        About Us
+
+        </p>
+            <div className="flex flex-row pb-20 text-gray-400">
+                <div>
+                    <p className="mt-8">
+                        For decades we have been more than a dealership, <br/>  we are curators of the extraodrdinary 
+                    </p>
+                    </div>
+
+                    <div className="mt-1 ml-50">
+
+                           <p>
+                       We believe luxury is not just about what you are drive,but about <br/>
+                        the experience it brings. That's why we go beyound selling cars<br/>
+                        we create a lifestyle of exclusivity,sophistication and <br/>
+                        unmatched quality 
+                    </p>
+                    </div>
+                </div>
+ 
+
+            </div>
+      
+        </>
+    )
+}
+export default About
